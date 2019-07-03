fdde31ea3005c144d52dfbb4b362fdd176127381
c2csg <37895518+c2csg@users.noreply.github.com>
2019-07-03 17:48:23 -0400

    remove chrome and vivaldi + add p7zip

--- install.sh
+++ install.sh
@@ -1,13 +1,9 @@
-Java Windscribe Play on linux Intellij need to be downloaded, 
+Java Windscribe Play on linux Intellij chrome vivaldi need to be downloaded, 
 sudo add-apt-repository -y ppa:teejee2008/ppa
 sudo add-apt-repository ppa:lutris-team/lutris
 sudo add-apt-repository universe
 sudo add-apt-repository ppa:alexlarsson/flatpak
-sudo apt install gnome-shell-extensions chrome-gnome-shell linuxbrew-wrapper exfat-fuse exfat-utils
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome-stable_current_amd64.deb
-wget -qO- http://repo.vivaldi.com/stable/linux_signing_key.pub | sudo apt-key add -
-sudo add-apt-repository "deb [arch=i386,amd64] http://repo.vivaldi.com/stable/deb/ stable main"
+sudo apt install gnome-shell-extensions chrome-gnome-shell linuxbrew-wrapper exfat-fuse exfat-utils p7zip-full p7zip-rar
 brew install hello atom python3 python2 youtube-dl adobe-photoshop-lightroom android-platform-tools android-messages git android-studio android-sdk balenaetcher youtube-dl ffmpeg ffsend
 sudo apt install timeshift ukuu lutris python2 python3 chromium steam playonlinux thunar lynx vivaldi-stablegnome-tweak-tool virtualbox geary gnome-themes-standard gtk2-engines-murrine libglib2.0-dev libxml2-utils materia-gtk-theme flatpak gnome-software-plugin-flatpak openjdk-11-jre gnome-themes-extra kalzium tillix gs-connect vlc typecatcher
 sudo snap install flameshot gimp atom --classic
