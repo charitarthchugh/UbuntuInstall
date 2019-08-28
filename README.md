3bff2c707485a98f323416adf90917d39205fc91
Charitarth <charitarth.chugh@gmail.com>
2019-08-27 22:28:02 -0400

    Delete remove.sh

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # UbuntuInstall
 Ubuntu Install script for my necessary apps
+Install script- install.sh
--- remove.sh
+++
@@ -1,1 +0,0 @@
-# I know this is not needed( will get the already installed error) but it is for personal satisfaction
