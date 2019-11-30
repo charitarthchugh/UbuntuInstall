af3a39bcf61c8103df08c35fc311694b845f331c
charitarthchugh <37895518+charitarthchugh@users.noreply.github.com>
2019-11-30 16:01:50 -0500

    Fix items
    remove vivaldi-stable,fix jdk, add apt update/upgrade

--- install.sh
+++ install.sh
@@ -1,11 +1,13 @@
-#Java Windscribe Play on linux  chrome vivaldi need to be downloaded, 
+#Oracle Java Windscribe Play on linux vivaldi chrome need to be downloaded
+sudo apt update -y
+sudo apt upgrade -y
 sudo add-apt-repository -y ppa:teejee2008/ppa
 sudo add-apt-repository ppa:lutris-team/lutris
 sudo add-apt-repository universe
 sudo add-apt-repository ppa:alexlarsson/flatpak
 sudo apt install gnome-shell-extensions chrome-gnome-shell linuxbrew-wrapper exfat-fuse exfat-utils p7zip-full p7zip-rar -y
 brew install hello gcc python3 python2 youtube-dl android-platform-tools android-messages git ffmpeg ffsend adoptopenjdk openjdk@12 howdoi
-sudo apt install timeshift ukuu lutris python2 python3 chromium steam playonlinux thunar lynx vivaldi-stable gnome-tweak-tool virtualbox geary gnome-themes-standard gtk2-engines-murrine libglib2.0-dev libxml2-utils materia-gtk-theme flatpak gnome-software-plugin-flatpak openjdk-11-jre openjdk-11-jdk gnome-themes-extra kalzium tillix gs-connect typecatcher -y
+sudo apt install timeshift ukuu lutris python2 python3 chromium steam playonlinux thunar lynx gnome-tweak-tool virtualbox geary gnome-themes-standard gtk2-engines-murrine libglib2.0-dev libxml2-utils materia-gtk-theme flatpak gnome-software-plugin-flatpak gnome-themes-extra kalzium tillix gs-connect typecatcher -y
 sudo snap install flameshot atom intellij-ultimate-classic kotlin code --classic
 sudo snap install gimp libreoffice vlc termius-app docker 
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
