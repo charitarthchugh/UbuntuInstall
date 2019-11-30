093681e227e0a3f1b201b770c0780414a6420e88
charitarthchugh <37895518+charitarthchugh@users.noreply.github.com>
2019-11-30 18:31:15 -0500

    add srcrcpy

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ sudo add-apt-repository ppa:lutris-team/lutris
 sudo add-apt-repository universe
 sudo add-apt-repository ppa:alexlarsson/flatpak
 sudo apt install gnome-shell-extensions chrome-gnome-shell linuxbrew-wrapper exfat-fuse exfat-utils p7zip-full p7zip-rar -y
-brew install hello gcc python python@2 youtube-dl android-platform-tools android-messages git ffmpeg ffsend adoptopenjdk openjdk@12 howdoi tomcat lazydocker docker
+brew install hello gcc python python@2 youtube-dl android-platform-tools android-messages git ffmpeg ffsend adoptopenjdk openjdk@12 howdoi tomcat scrcpy lazydocker docker
 sudo apt install timeshift ukuu lutris python2 python3 chromium steam playonlinux thunar lynx gnome-tweak-tool virtualbox geary gnome-themes-standard gtk2-engines-murrine libglib2.0-dev libxml2-utils materia-gtk-theme flatpak gnome-software-plugin-flatpak gnome-themes-extra kalzium tillix gs-connect typecatcher -y
 sudo snap install flameshot atom intellij-ultimate-classic kotlin code --classic
 sudo snap install gimp libreoffice vlc termius-app 
