52dd2bea3edb467a59cba617b4048bdf53504bb4
charitarthchugh <37895518+charitarthchugh@users.noreply.github.com>
2019-11-30 19:46:42 -0500

    add adb

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ sudo add-apt-repository universe
 sudo add-apt-repository ppa:alexlarsson/flatpak
 sudo apt install gnome-shell-extensions chrome-gnome-shell linuxbrew-wrapper exfat-fuse exfat-utils p7zip-full p7zip-rar -y
 brew install hello gcc python python@2 youtube-dl android-platform-tools android-messages git ffmpeg ffsend adoptopenjdk openjdk@12 howdoi tomcat scrcpy lazydocker docker
-sudo apt install timeshift ukuu lutris python2 python3 chromium steam playonlinux thunar lynx gnome-tweak-tool virtualbox geary gnome-themes-standard gtk2-engines-murrine libglib2.0-dev libxml2-utils materia-gtk-theme flatpak gnome-software-plugin-flatpak gnome-themes-extra kalzium tillix gs-connect typecatcher -y
+sudo apt install timeshift ukuu lutris python2 python3 chromium steam playonlinux thunar lynx gnome-tweak-tool virtualbox geary gnome-themes-standard gtk2-engines-murrine libglib2.0-dev libxml2-utils materia-gtk-theme flatpak gnome-software-plugin-flatpak gnome-themes-extra kalzium tillix gs-connect typecatcher adb -y
 sudo snap install flameshot atom intellij-ultimate-classic kotlin code --classic
 sudo snap install gimp libreoffice vlc termius-app 
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
