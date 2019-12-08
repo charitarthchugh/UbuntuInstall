5c77351179c646a851e02ea338e91cc470a8b859
charitarthchugh <37895518+charitarthchugh@users.noreply.github.com>
2019-12-08 10:27:48 -0500

    fix errors
    remove docker, lazydocker
    remove ukuu (now paid software)
    remove gs-connect)(have to do that manually)
    remove gs-connect(need to do it manually)
    fix homebrew install, chromium
    add methods to install oracle jdk,chrome, vivaldi

--- install.sh
+++ install.sh
@@ -1,18 +1,30 @@
-#Oracle Java Windscribe Play on linux vivaldi chrome need to be downloaded
 sudo apt update -y
 sudo apt upgrade -y
 sudo add-apt-repository -y ppa:teejee2008/ppa
 sudo add-apt-repository ppa:lutris-team/lutris
 sudo add-apt-repository universe
 sudo add-apt-repository ppa:alexlarsson/flatpak
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key FDC247B7
+echo 'deb https://repo.windscribe.com/ubuntu bionic main'
 sudo apt install gnome-shell-extensions chrome-gnome-shell linuxbrew-wrapper exfat-fuse exfat-utils p7zip-full p7zip-rar -y
-brew install hello gcc python python@2 youtube-dl android-platform-tools android-messages git ffmpeg ffsend adoptopenjdk openjdk@12 howdoi tomcat scrcpy lazydocker docker
-sudo apt install timeshift ukuu lutris python2 python3 chromium steam playonlinux thunar lynx gnome-tweak-tool virtualbox geary gnome-themes-standard gtk2-engines-murrine libglib2.0-dev libxml2-utils materia-gtk-theme flatpak gnome-software-plugin-flatpak gnome-themes-extra kalzium tillix gs-connect typecatcher adb -y
-sudo snap install flameshot atom intellij-ultimate-classic kotlin code --classic
-sudo snap install gimp libreoffice vlc termius-app 
+echo "export Path=/usr/local/bin:$PATH" >> ~/.bash_profile && source  ~/.bash_profile
+git -C "(brew --repo homebrew/core)" fetch --unshallow
+brew install hello gcc python python@2 youtube-dl git ffsend adoptopenjdk openjdk@12 howdoi scrcpy 
+sudo apt install timeshift windscribe-cli lutris python2 python3 steam playonlinux thunar gnome-tweak-tool virtualbox geary gnome-themes-standard gtk2-engines-murrine libglib2.0-dev libxml2-utils materia-gtk-theme flatpak gnome-software-plugin-flatpak gnome-themes-extra tilix typecatcher adb -y
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+wget https://downloads.vivaldi.com/stable/vivaldi-stable_2.9.1705.41-1_amd64.deb
+sudo dpkg -i vivaldi-stable_2.9.1705.41-1_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+wget https://download.oracle.com/otn-pub/java/jdk/13.0.1+9/cec27d702aa74d5a8630c65ae61e4305/jdk-13.0.1_linux-x64_bin.deb
+sudo dpkg -i jdk-13.0.1_linux-x64_bin.deb
+sudo snap install flameshot --classic 
+sudo snap install intellij-idea-ultimate --classic
+sudo snap install kotlin --classic
+sudo snap install code --classic
+sudo snap install gimp vlc termius-app chromium
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-flatpak install flathub org.gtk.Gtk3theme.Materia{,-dark,-light}{,-compact}
-sudo apt update
-sudo apt upgrade
-sudo apt autoremove
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt autoremove -y
+echo https://extensions.gnome.org/extension/1319/gsconnect/
