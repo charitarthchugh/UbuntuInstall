ecbb1eddec45bbb06087767c1a91773cc01b8581
charitarthchugh <37895518+charitarthchugh@users.noreply.github.com>
2020-05-03 15:07:56 -0400

    Quality of life improvents

--- install.sh
+++ install.sh
@@ -15,8 +15,10 @@ sudo add-apt-repository universe -y
 sudo add-apt-repository ppa:tista/adapta -y
 sudo apt-add-repository ppa:noobslab/deepin-sc -y
 sudo add-apt-repository ppa:alexlarsson/flatpak -y
+ sudo add-apt-repository ppa:bashtop-monitor/bashtop
 sudo dpkg --add-architecture i386 #if it is not enabled already
-wget https://download.opensuse.org/repositories/Emulators:/Wine:/Debian/xUbuntu_19.04/amd64/libfaudio0_20.01-0~disco_amd64.deb #dependency for Wine-libfaudio
+# install snapd-may fail
+sudo apt install -y snapd
 sudo apt install python-nautilus gir1.2-nautilus-3.0 gir1.2-ebook-1.2 git hub gir1.2-ebookcontacts-1.2 gir1.2-edataserver-1.2 curl file gcc build-essential -y
 sudo apt install gnome-shell-extensions chrome-gnome-shell exfat-fuse exfat-utils p7zip-full p7zip-rar -y
 #homebrew section
@@ -26,12 +28,14 @@ test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew
 test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
 echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
 echo 'export PATH="/home/2p2107032/.linuxbrew/bin:$PATH"' >> ~/.bash_profile
-brew
+brew install hello
 echo "export Path=/usr/local/bin:$PATH" >> ~/.bash_profile && source  ~/.bash_profile#homebrew post install
 git -C "(brew --repo homebrew/core)" fetch --unshallow
-brew install gcc python python@2 youtube-dl git ffsend openjdk@10 openjdk howdoi scrcpy maven oracle-jdk-javadoc kotlin
+brew install gcc python python@2 youtube-dl ffsend howdoi scrcpy maven kotlin
 #main package install
-sudo apt install timeshift lutris python2 python3 playonlinux thunar gnome-tweak-tool adapta vlc gimp darktable gdebi npm chromium autojump glances geary gnome-themes-standard gtk2-engines-murrine libglib2.0-dev libxml2-utils materia-gtk-theme flatpak gnome-software-plugin-flatpak gnome-themes-extra tilix flameshot vim nano  -y
+sudo apt install -y timeshift python2 python3 playonlinux thunar gnome-tweak-tool adapta vlc gimp darktable gdebi npm glances geary
+sudo apt install -y gnome-themes-standard gtk2-engines-murrine libglib2.0-dev libxml2-utils materia-gtk-theme
+sudo apt install -y flatpak gnome-software-plugin-flatpak gnome-themes-extra tilix flameshot vim nano bashtop
 #Install Browsers + oracle jdk
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 wget https://downloads.vivaldi.com/stable/vivaldi-stable_2.9.1705.41-1_amd64.deb
@@ -41,9 +45,8 @@ wget "https://download.oracle.com/otn-pub/java/jdk/13.0.2+8/d4173c853231432d94f0
 sudo dpkg -i *.deb
 #IntelliJ download
 sudo snap install intellij-idea-ultimate --classic
-sudo apt install -y default-jdk default-jre#for openjdk/jre through apt
-# install snapd-may fail
-sudo apt install -y snapd
+# Java JDK install
+sudo apt install -y default-jdk default-jre openjdk-14-jdk openjdk-13-jdk openjdk-8-jdk openjdk-8-jre openjdk-11-jdk openjfx
 sudo apt install -y gnome-boxes dart maven gradle
 #Fonts Section
 sudo apt install -y fonts-noto fonts-lato fonts-roboto fonts-materialdesignicons-webfont fonts-hack-otf fonts-powerline typecatcher fonts-firacode font-manager msttcorefonts fonts-indic fonts-wine
@@ -88,8 +91,7 @@ wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/ProggyClea
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Overpass.zip
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Terminus.zip
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Ubuntu.zip
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/LiberationMono.zip
-unzip *.zip
+`for f in *.zip; do unzip -d "${f%*.zip}" "$f"; done`
 rm *.zip -f
 mv *.otf ~/.fonts/
 mv *.ttf ~/.fonts/
@@ -97,11 +99,10 @@ git clone https://github.com/theleagueof/raleway ~/.fonts/Raleway
 npm install --save @fortawesome/fontawesome-free
 cd ~
 fc-cache -f -v
-./usr/share/autojump/autojump.sh#autojump init script
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 #Atom+ sublime install
-apm install terminal-plus java-plus ide-java java-import-wiz atom-material-ui snazzy-syntax java-generator atom-maven deepcode formatter-clangformat
+apm install java-plus ide-java java-import-wiz atom-material-ui snazzy-syntax java-generator atom-maven deepcode formatter-clangformat
 #tilix
 cd ~/.config/tilix/schemes/
 wget -q https://github.com/clrxbl/tilix-snazzy/raw/master/snazzy.json
@@ -111,10 +112,11 @@ wget -qO "~/.config/tilix/schemes/monokai.json" https://git.io/v7Qad
 wget -qO "~/.config/tilix/schemes/one-dark.json" https://git.io/v7Qaw
 cd ~
 # Zsh section
-sudo apt install zsh zsh-syntax-highlighting antigen -y
+sudo apt install zsh zsh-syntax-highlighting antigen autojump -y
 https://raw.githubusercontent.com/charitarthchugh/UbuntuInstall/master/.zshrc
 git clone https://github.com/sindresorhus/pure.git "$HOME/.zsh/pure"#Pure Prompt, npm does not work all the time
 wget https://raw.githubusercontent.com/charitarthchugh/UbuntuInstall/master/aliasrc -O ~/.zsh/aliasrc #get my alaisrc
+./usr/share/autojump/autojump.sh#autojump init script
 #Wine Section
 wget -nc https://dl.winehq.org/wine-builds/winehq.key
 sudo apt-key add winehq.key
@@ -124,10 +126,11 @@ sudo apt install --install-recommends -y winehq-staging
 sudo apt update -y
 sudo apt install --install-recommends -y winehq-staging
 sudo update-alternatives --config x-terminal-emulator
-#Final Steps
+#Install GNOME shell extentions
+sudo apt install -y gnome-shell-extension-caffeine gnome-shell-extension-gsconnect gnome-shell-extension-gsconnext-browsers
+sudo apt install -y gnome-shell-extension-no-annoyance gnome-shell-extension-multi-monitors gnome-shell-extension-bluetooth-quick-connect
+# Final Steps
 sudo apt update -y
 sudo apt upgrade -y
 figlet -c "2CC Ubuntu Install Script End"
 sudo apt autoremove -y
-firefox https://extensions.gnome.org/extension/1319/gsconnect/
-chrome https://extensions.gnome.org/extension/1319/gsconnect/
