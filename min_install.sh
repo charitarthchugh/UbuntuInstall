ad71650320829968fcca5d3f9675741fa3fd6928
charitarthchugh <37895518+charitarthchugh@users.noreply.github.com>
2020-07-31 13:18:59 -0400

    Update min_install.sh
    fix antibody, vim install, remove aliases, remove  pure cloneing, youcompleteme install
    remove duplicate unzip

--- min_install.sh
+++ min_install.sh
@@ -1,24 +1,22 @@
 #!/bin/bash
 sudo apt update -y
 sudo apt upgrade -y
-alias APTI ="sudo apt install -y"
-alias APTREPO = "sudo add-apt-repository -y"
-alias snapI= "sudo snap install"
 sudo dpkg --add-architecture i386
 #Repositories
-APTREPO ppa:teejee2008/ppa
-APTREPO ppa:bashtop-monitor/bashtop
+sudo add-apt-repository -y ppa:teejee2008/ppa
+sudo add-apt-repository -y ppa:bashtop-monitor/bashtop
 # Main apt install
-APTI timeshift bashtop
-APTI curl gcc file build-essential hub default-jdk default-jre flatpak zsh exfat-fuse exfat-utils p7zip-full p7zip-rar 
-APTI unzip tar snapd trash-cli vim openssh-server unzip 
+sudo apt install snapd
+sudo apt install -y timeshift bashtop
+sudo apt install -y curl gcc file build-essential hub lolcat default-jdk default-jre flatpak zsh exfat-fuse exfat-utils p7zip-full p7zip-rar 
+sudo apt install -y unzip tar snapd trash-cli vim openssh-server npm golang python3-dev cmake npm python3-psutil python3-pip
 #Install brew
 bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)" 
 echo 'export PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"' >> ~/.bashrc
 git -C "(brew --repo homebrew/core)" fetch --unshallow
 # brew apps
 brew install gcc python npm topgrade 
-brew install antibody
+curl -sfL git.io/antibody | sudo  sh -s - -b /usr/local/bin
 #flatpak setup
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
@@ -27,4 +25,7 @@ git clone https://github.com/charitarthchugh/dotfiles ~/dotfiles/
 cp -s ~/dotfiles/.zshrc ~
 cp -s ~/dotfiles/.vimrc ~
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim# Vundle
-git clone https://github.com/sindresorhus/pure.git "$HOME/.zsh/pure"
+mkdir -p ~/.vim/colors
+cd ~/.vim/colors && wget https://raw.githubusercontent.com/connorholyday/vim-snazzy/master/colors/snazzy.vim
+cd ~
+python ~/.vim/bundle/youcompleteme/install.sh --all
