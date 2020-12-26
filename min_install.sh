ddc83c7abd35fa6e0b7a02a1b3db0ff22a328fba
charitarthchugh <37895518+charitarthchugh@users.noreply.github.com>
2020-12-25 23:22:33 -0500

    Update min_install.sh

--- min_install.sh
+++ min_install.sh
@@ -4,19 +4,18 @@ sudo apt upgrade -y
 sudo dpkg --add-architecture i386
 #Repositories
 sudo add-apt-repository -y ppa:teejee2008/ppa
-sudo add-apt-repository -y ppa:bashtop-monitor/bashtop
 # Main apt install
 sudo apt install snapd
-sudo apt install -y timeshift bashtop
-sudo apt install -y curl gcc file build-essential hub lolcat default-jdk default-jre flatpak zsh exfat-fuse exfat-utils p7zip-full p7zip-rar 
+sudo apt install -y timeshift
+sudo apt install -y curl gcc file build-essential hub lolcat default-jdk default-jre flatpak zsh exfat-fuse exfat-utils p7zip-full p7zip-rar
 sudo apt install -y unzip tar snapd trash-cli vim openssh-server npm golang python3-dev cmake npm python3-psutil python3-pip
 #Install brew
-bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)" 
-echo 'export PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"' >> ~/.bashrc
+bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+echo 'export PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"' >>~/.bashrc
 git -C "(brew --repo homebrew/core)" fetch --unshallow
 # brew apps
-brew install gcc python npm topgrade 
-curl -sfL git.io/antibody | sudo  sh -s - -b /usr/local/bin
+brew install gcc python npm topgrade
+curl -sfL git.io/antibody | sudo sh -s - -b /usr/local/bin
 #flatpak setup
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
