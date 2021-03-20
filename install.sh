28996b47be3947fa52c8827f27e04cd74cf1c11a
charitarthchugh <37895518+charitarthchugh@users.noreply.github.com>
2021-03-20 10:50:51 -0400

    update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
-# Enable 32 bit architecture
-sudo dpkg --add-architecture i386
 # Update all installed packages
 sudo apt update -y
 sudo apt upgrade -y
+sudo apt autoremove -y
 # Add repositories for apt
 sudo apt install apt-transport-https curl gnupg software-properties-common # required to do before main application install
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key C99B11DEB97541F0
@@ -13,13 +12,22 @@ curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt
 
 echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-if [ ! -f /etc/apt/sources.list.d/system76-ubuntu-pop-focal.list ];then #make sure that it is not pop-os
+source /etc/os-release
+if [ ! $ID = 'pop' ];then #make sure that it is not pop-os
     sudo add-apt-repository ppa:mmstick76/alacritty
+else
+    echo "System76 Packages Alacritty, not adding ppa"
 fi  
+# Wine
+# Enable 32 bit architecture
+sudo dpkg --add-architecture i386
+wget -nc https://dl.winehq.org/wine-builds/winehq.key
+sudo apt-key add winehq.key
+sudo add-apt-repository sudo add-apt-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ '$VERSION_CODENAME' main'
 sudo add-apt-repository -y ppa:teejee2008/ppa
 sudo add-apt-repository -y ppa:git-core/ppa
 sudo add-apt-repository -y ppa:phoerious/keepassxc
-sudo apt update  
+sudo apt update -y
 # Programs to install via apt
 xargs --arg-file="~/fre.sh/apt_packages.txt" sudo apt install -y
 
@@ -35,12 +43,13 @@ cd $HOME/Dev/GTK
 git clone https://github.com/vinceliuice/WhiteSur-gtk-theme
 git clone https://github.com/vinceliuice/WhiteSur-icon-theme 
 git clone https://github.com/vinceliuice/WhiteSur-cursors
-sudo ./WhiteSur-gtk-theme/install.sh -g -c dark -s 200
+sudo ./WhiteSur-gtk-theme/install.sh -g -c dark -i ubuntu
 ./WhiteSur-icon-theme/install.sh 
 ./WhiteSur-cursors/install.sh
 gsettings set org.gnome.desktop.interface icon-theme "WhiteSur-dark"
 gsettings set org.gnome.desktop.interface gtk-theme "WhiteSur-dark"
 gsettings set org.gnome.desktop.interface cursor-theme "WhiteSur-dark"
+sudo flatpak override --filesystem=/usr/share/themes/WhiteSur-dark/
 cd $HOME
 # Setup dotfiles
 curl -sfL git.io/antibody | sudo sh -s - -b /usr/local/bin
